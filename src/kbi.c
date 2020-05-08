a3a40fac2c81aac114b0960a3d69594b0e6af3f0
Stephen Cochrane <skippycochrane@gmail.com>
2020-05-08 10:46:34 +0200

    Loops working, must test a bit more

--- src/kbi.c
+++ src/kbi.c
@@ -5,15 +5,16 @@ int main(int argc, char *argv[]) {
 	FILE *fp;
 	char *buffer;
 	int index, instr, windex, *wpos, wpos_sz, buff_sz;
-	int *ptr;
+	int *ptr, *pf;
 
 	index = 0;
-	windex = 0;
+	windex = -1;
 	wpos = malloc(sizeof(int));
-	wpos_sz = 1;
+	wpos_sz = 2;
 	buffer = malloc(sizeof(char)*256);
 	buff_sz = 256;
 	ptr = calloc(10000, sizeof(int));
+	pf = ptr;
 
 	fp = fopen(argv[1], "r");
 
@@ -56,14 +57,26 @@ int main(int argc, char *argv[]) {
 				ptr--;
 				break;
 			case '[':
+				windex++;
+				if (windex == wpos_sz) {
+					wpos_sz *= 2;
+					wpos = realloc(wpos, sizeof(int)*wpos_sz);
+				}
+				wpos[windex] = index;
 				break;
 			case ']':
+				if (*ptr) {
+					//restart loop
+					index = wpos[windex];
+				} else {
+					windex--;
+				}
 				break;
 		}
 		instr = buffer[++index];
 	}
 	
-
+	free(pf);
 	free(buffer);
 	free(wpos);
 	return EXIT_SUCCESS;
